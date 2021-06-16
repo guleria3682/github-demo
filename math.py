8764f5f343a1d0ee2500f451d13d3cfc37235d21
Mercury <himanshuguleria0694@gmail.com>
2021-06-16 15:02:26 +0530

    Added function header

--- math.py
+++ math.py
@@ -1,12 +1,12 @@
-
+# Add Implementation
 def add(x,y):
     pass
-
+# Subtract Implementation
 def subtract(x,y):
     pass
-
+# Multiply Implementation
 def multiply(x,y):
     pass
-
+# Divisio Implimentation
 def divide(x,y):
    pass
